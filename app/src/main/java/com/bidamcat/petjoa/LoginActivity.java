8fdf7c28db34a8367b6d5ff078437f02b7e14167
kimbidam <zisu55@naver.com>
2021-09-08 15:10:00 +0900

    login -> home

--- app/src/main/java/com/bidamcat/petjoa/LoginActivity.java
+++ app/src/main/java/com/bidamcat/petjoa/LoginActivity.java
@@ -2,6 +2,7 @@ package com.bidamcat.petjoa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,11 +12,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
 
     public void clickJoin(View view) {
     }
 
     public void clickLogin(View view) {
+        Intent intent= new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(intent);
+
+
     }
 }
