7bb25d620197e9a18b841e432bfce4bc982909f1
kimbidam <zisu55@naver.com>
2021-09-10 14:31:40 +0900

    img make

--- app/src/main/java/com/bidamcat/petjoa/CatImgMakeActivity.java
+++ app/src/main/java/com/bidamcat/petjoa/CatImgMakeActivity.java
@@ -3,6 +3,7 @@ package com.bidamcat.petjoa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class CatImgMakeActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class CatImgMakeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_img_make);
+    }
+
+    public void click_ImgUpLoad(View view) {
+    }
+
+    public void click_Ok(View view) {
+    }
+
+    public void click_cancel(View view) {
     }
 }
