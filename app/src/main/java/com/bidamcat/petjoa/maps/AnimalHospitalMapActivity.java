368911aad4319d50021caad453c3375c807f2868
kimbidam <zisu55@naver.com>
2021-09-17 14:57:29 +0900

    0917

---
+++ app/src/main/java/com/bidamcat/petjoa/maps/AnimalHospitalMapActivity.java
@@ -0,0 +1,16 @@
+package com.bidamcat.petjoa.maps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bidamcat.petjoa.R;
+
+public class AnimalHospitalMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animal_hospital_map);
+    }
+}
