c80a7fa49b151285e680ceccf59925c0f42466cc
kimbidam <zisu55@naver.com>
2021-09-28 18:38:18 +0900

    0928 2

---
+++ app/src/main/java/com/bidamcat/petjoa/items/DogIFMItem.java
@@ -0,0 +1,21 @@
+package com.bidamcat.petjoa.items;
+
+public class DogIFMItem {
+
+    public int no;
+    public String name;
+    public String title;
+    public String msg;
+    public String date;
+
+    public DogIFMItem() {
+    }
+
+    public DogIFMItem(int no, String name, String title, String msg, String date) {
+        this.no = no;
+        this.name = name;
+        this.title = title;
+        this.msg = msg;
+        this.date = date;
+    }
+}
