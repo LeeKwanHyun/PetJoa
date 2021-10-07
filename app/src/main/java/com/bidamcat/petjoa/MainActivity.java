dfa5319c01e8fccb062c53088af1891ab4846668
kimbidam <zisu55@naver.com>
2021-10-07 19:58:03 +0900

    1007

--- app/src/main/java/com/bidamcat/petjoa/MainActivity.java
+++ app/src/main/java/com/bidamcat/petjoa/MainActivity.java
@@ -3,6 +3,9 @@ package com.bidamcat.petjoa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.kakao.sdk.common.util.Utility;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
